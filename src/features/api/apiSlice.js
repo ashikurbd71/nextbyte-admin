48ded32540125818dcf658f594c7c3fd431683f7
ashikurovibd71 <ashikurovi2003@gmail.com>
2025-12-14 18:19:17 +0600

    update

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,7 +5,7 @@ import {
 } from "@/features/adminauth/adminAuthSlice";
 import { getTokens } from "@/hooks/useToken";
 
-const BASE_URL = 'https://api.nextbyteitinstitute.com/';
+const BASE_URL = 'https://server.private.nextbyteitinstitute.com';
 
 // base fetch query
 const baseQuery = fetchBaseQuery({
